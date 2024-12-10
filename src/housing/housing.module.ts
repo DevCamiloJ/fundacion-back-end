34a1cbca699ce3c29723f8756518ce6321bfe571
Kamilo1205 <kamilo_1205@outlook.com>
2024-12-10 13:42:37 -0500

    fix(housting): :bug: agregar importación faltante de TypeOrmModule con la entidad Housing

--- src/housing/housing.module.ts
+++ src/housing/housing.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { HousingService } from './housing.service';
 import { HousingController } from './housing.controller';
+import { Housing } from './entities/housing.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Housing])],
   controllers: [HousingController],
   providers: [HousingService],
+  exports: [HousingService]
 })
 export class HousingModule {}
