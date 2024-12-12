133cf7d354934523afe3ac35bba860f77ecabfb2
Kamilo1205 <kamilo_1205@outlook.com>
2024-12-12 10:30:17 -0500

    fix(members): :bug: agregar importaciones de los modulos de areas de interes y discapacidades

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { InterestTopicModule } from 'src/interest-topic/interest-topic.module';
+import { DisabilityModule } from 'src/disability/disability.module';
 
 import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
 import { Member } from './entities/member.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [
+    TypeOrmModule.forFeature([Member]),
+    InterestTopicModule,
+    DisabilityModule
+  ],
   controllers: [MembersController],
   providers: [MembersService],
 })
